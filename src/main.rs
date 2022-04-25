b61a00ec6aa602c6abdf2705cc0c922662421881
Cendyne <cendyne@cendyne.dev>
2022-04-25 18:25:45 -0500

    Simplify handlers, add custom push handler

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use dashmap::DashMap;
 use http::{Response, StatusCode};
+use once_cell::sync::OnceCell;
 use serde::{Deserialize, Serialize};
-use std::borrow::Cow;
 use std::convert::Infallible;
 use std::env;
 use std::sync::{Arc, Mutex};
@@ -9,6 +9,50 @@ use tokio::fs::{read_to_string, write};
 use warp::{Filter, Rejection, Reply};
 
 const ALPHABET: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+static CORRECT_BEARER_TOKEN_HASH: OnceCell<[u8; blake3::OUT_LEN]> = OnceCell::new();
+static CHOSEN_ALPHABET: OnceCell<String> = OnceCell::new();
+static DATA_SOURCE: OnceCell<String> = OnceCell::new();
+static SETTINGS: OnceCell<Arc<JsonSettings>> = OnceCell::new();
+
+fn load_correct_bearer_token_hash() -> Result<[u8; blake3::OUT_LEN], String> {
+    let input = std::env::var("TOKEN").map_err(|e| format!("{}", e))?;
+    let correct_bearer = format!("Bearer {}", input);
+    let mut hasher = blake3::Hasher::new();
+    hasher.update(correct_bearer.as_bytes());
+    let hash = hasher.finalize();
+    let hash_bytes = hash.as_bytes();
+    Ok(*hash_bytes)
+}
+
+fn chosen_alphabet() -> &'static str {
+    CHOSEN_ALPHABET.get_or_init(|| {
+        env::var("ALPHABET")
+            .ok()
+            .unwrap_or_else(|| ALPHABET.to_string())
+    })
+}
+
+fn data_source() -> &'static str {
+    DATA_SOURCE.get_or_init(|| {
+        env::var("DATA")
+            .ok()
+            .unwrap_or_else(|| "data.json".to_string())
+    })
+}
+
+async fn load_json_settings() -> Arc<JsonSettings> {
+    match SETTINGS.get() {
+        None => {
+            let data = load_data(data_source()).await;
+            match SETTINGS.try_insert(data.clone()) {
+                Ok(_) => data,
+                Err((actual, _)) => actual.clone(),
+            }
+        }
+        Some(data) => data.clone(),
+    }
+}
 
 fn increment_string(alphabet: &str, symbols: &str) -> String {
     let mut result: Vec<char> = Vec::with_capacity(symbols.len() + 1);
@@ -23,8 +67,8 @@ fn increment_string(alphabet: &str, symbols: &str) -> String {
                     result.push(first);
                 }
             } else if let Some(next) = alphabet.chars().nth(value + 1) {
-                    result.push(next);
-                    carry = 0;
+                result.push(next);
+                carry = 0;
             }
         } else {
             println!("Unsupported character {}, using last", c);
@@ -63,7 +107,6 @@ async fn load_data(source: &str) -> Arc<JsonSettings> {
         Err(e) => {
             println!("Could not load settings: {}", e);
             let redirects: DashMap<String, String> = DashMap::with_capacity(80);
-            redirects.insert("".to_string(), "https://cendyne.dev/".to_string());
             let json = JsonSettings {
                 redirects,
                 last_symbol: Mutex::new("".to_string()),
@@ -74,24 +117,6 @@ async fn load_data(source: &str) -> Arc<JsonSettings> {
     }
 }
 
-fn with_settings(
-    settings: Arc<JsonSettings>,
-) -> impl Filter<Extract = (Arc<JsonSettings>,), Error = Infallible> + Clone {
-    warp::any().map(move || settings.clone())
-}
-
-fn with_alphabet(
-    alphabet: Cow<String>,
-) -> impl Filter<Extract = (Cow<String>,), Error = Infallible> + Clone {
-    warp::any().map(move || alphabet.clone())
-}
-
-fn with_source(
-    source: Cow<String>,
-) -> impl Filter<Extract = (Cow<String>,), Error = Infallible> + Clone {
-    warp::any().map(move || source.clone())
-}
-
 async fn save_data(settings: &JsonSettings, source: &str) -> Result<(), String> {
     let serialized = serde_json::to_string(&settings).map_err(|e| format!("{}", e))?;
     write(source, serialized)
@@ -100,8 +125,11 @@ async fn save_data(settings: &JsonSettings, source: &str) -> Result<(), String> 
     Ok(())
 }
 
-fn redirect_handler(name: String, settings: Arc<JsonSettings>) -> http::Result<impl Reply> {
-    if let Some(path) = settings.redirects.get(&name) {
+async fn redirect_handler(name: String) -> http::Result<impl Reply> {
+    let settings = load_json_settings().await;
+    let found_path = settings.redirects.get(&name);
+    // Cannot embed redirects get into the if let below due to borrow lifetimes
+    if let Some(path) = found_path {
         Response::builder()
             .status(StatusCode::FOUND)
             .header("Location", path.clone())
@@ -113,8 +141,8 @@ fn redirect_handler(name: String, settings: Arc<JsonSettings>) -> http::Result<i
     }
 }
 
-fn empty_redirect_handler(settings: Arc<JsonSettings>) -> http::Result<impl Reply> {
-    redirect_handler("".to_string(), settings)
+async fn empty_redirect_handler() -> http::Result<impl Reply> {
+    redirect_handler("".to_string()).await
 }
 
 fn next_symbol(settings: &JsonSettings, alphabet: &str) -> Option<String> {
@@ -146,43 +174,52 @@ fn add_redirect(settings: &JsonSettings, key: &str, value: &str) {
         .insert(key.to_string(), value.to_string());
 }
 
-async fn push_handler(
-    settings: Arc<JsonSettings>,
-    authorization: String,
-    alphabet: Cow<'_, String>,
-    destination: String,
-    source: Cow<'_, String>,
-) -> Result<impl Reply, Infallible> {
-    if let Ok(token) = env::var("TOKEN") {
-        // This isn't great
-        if format!("Bearer {}", token) == authorization {
-            // Separate the mutex from the rest of the processing
-            let symbol = next_symbol(&settings, &alphabet);
-            if let Some(key) = symbol {
-                add_redirect(&settings, &key, &destination);
-                if save_data(&settings, &source).await.is_ok() {
-                    Ok(Response::builder()
-                    .status(StatusCode::OK)
-                    .body(format!("/{}", key)))
-                } else {
-                    Ok(Response::builder()
-                    .status(StatusCode::INTERNAL_SERVER_ERROR)
-                    .body("Could not save".to_string()))
-                }
-            } else {
-                Ok(Response::builder()
-                    .status(StatusCode::INTERNAL_SERVER_ERROR)
-                    .body("Server is poisoned".to_string()))
-            }
+fn check_authorization(authorization: String) -> Result<(), String> {
+    let correct_hash = CORRECT_BEARER_TOKEN_HASH.get_or_try_init(load_correct_bearer_token_hash)?;
+    let mut hasher = blake3::Hasher::new();
+    hasher.update(authorization.as_bytes());
+    let hash = hasher.finalize();
+    let hash_bytes = hash.as_bytes();
+    if constant_time_eq::constant_time_eq_32(correct_hash, hash_bytes) {
+        Ok(())
+    } else {
+        Err("Authorization failed".to_string())
+    }
+}
+
+async fn push_handler(destination: String) -> Result<impl Reply, Infallible> {
+    let settings = load_json_settings().await;
+    let alphabet = chosen_alphabet();
+    let symbol = next_symbol(&settings, alphabet);
+    if let Some(key) = symbol {
+        add_redirect(&settings, &key, &destination);
+        if save_data(&settings, data_source()).await.is_ok() {
+            Ok(Response::builder()
+                .status(StatusCode::OK)
+                .body(format!("/{}", key)))
         } else {
             Ok(Response::builder()
-                .status(StatusCode::UNAUTHORIZED)
-                .body("Bad Authentication".to_string()))
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                .body("Could not save".to_string()))
         }
     } else {
         Ok(Response::builder()
             .status(StatusCode::INTERNAL_SERVER_ERROR)
-            .body("TOKEN not set".to_string()))
+            .body("Server is poisoned".to_string()))
+    }
+}
+
+async fn push_custom_handler(path: String, destination: String) -> Result<impl Reply, Infallible> {
+    let settings = load_json_settings().await;
+    add_redirect(&settings, &path, &destination);
+    if save_data(&settings, data_source()).await.is_ok() {
+        Ok(Response::builder()
+            .status(StatusCode::OK)
+            .body(format!("/{}", path)))
+    } else {
+        Ok(Response::builder()
+            .status(StatusCode::INTERNAL_SERVER_ERROR)
+            .body("Could not save".to_string()))
     }
 }
 
@@ -191,12 +228,49 @@ struct CouldNotParsePlainText;
 
 impl warp::reject::Reject for CouldNotParsePlainText {}
 
+#[derive(Debug)]
+struct BadAuthorization(String);
+
+impl warp::reject::Reject for BadAuthorization {}
+
 pub fn plaintext() -> impl Filter<Extract = (String,), Error = Rejection> + Copy {
-    warp::filters::body::bytes().and_then(|buf: bytes:: Bytes| async move {
-        String::from_utf8(buf.to_vec()).map_err(|_| {
-            warp::reject::custom(CouldNotParsePlainText)
-        })
+    warp::filters::body::bytes().and_then(|buf: bytes::Bytes| async move {
+        String::from_utf8(buf.to_vec()).map_err(|_| warp::reject::custom(CouldNotParsePlainText))
     })
+}
+
+fn check_auth() -> impl Filter<Extract = (), Error = Rejection> + Copy {
+    warp::any()
+        .and(warp::header::<String>("authorization"))
+        .and_then(|auth| async move {
+            if let Err(message) = check_authorization(auth) {
+                Err(warp::reject::custom(BadAuthorization(message)))
+            } else {
+                Ok(())
+            }
+        })
+        .untuple_one()
+}
+
+async fn handle_rejection(err: Rejection) -> Result<impl Reply, Rejection> {
+    if err.is_not_found() {
+        Ok(warp::reply::with_status(
+            "NOT FOUND".to_string(),
+            StatusCode::NOT_FOUND,
+        ))
+    } else if let Some(BadAuthorization(message)) = err.find() {
+        Ok(warp::reply::with_status(
+            message.clone(),
+            StatusCode::UNAUTHORIZED,
+        ))
+    } else if let Some(CouldNotParsePlainText) = err.find() {
+        Ok(warp::reply::with_status(
+            "Input does not appear to be utf8".to_string(),
+            StatusCode::BAD_REQUEST,
+        ))
+    } else {
+        Err(err)
+    }
 }
 
 #[tokio::main]
@@ -208,45 +282,36 @@ async fn main() {
         }
     }
 
-    let alphabet: String = env::var("ALPHABET")
-        .ok()
-        .unwrap_or_else(|| ALPHABET.to_string());
-    let owned_alphabet = Cow::Owned(alphabet);
     let port = env::var("PORT")
         .ok()
         .and_then(|p| p.parse::<u16>().ok())
         .unwrap_or(8080);
-    let data_source = env::var("DATA")
-        .ok()
-        .unwrap_or_else(|| "data.json".to_string());
-    let data = load_data(&data_source).await;
-    let owned_data_source = Cow::Owned(data_source);
+    load_json_settings().await;
 
     let redirect = warp::get()
-        .and(
-            warp::path!(String)
-                .and(with_settings(data.clone()))
-                .map(redirect_handler),
-        )
-        .or(warp::get()
-            .and(with_settings(data.clone()))
-            .map(empty_redirect_handler));
-    let authorization = warp::header::<String>("authorization");
-    let push = warp::put().and(warp::path::end())
-        .and(warp::body::content_length_limit(4096)
-                .and(with_settings(data.clone()))
-                .and(authorization)
-                .and(with_alphabet(owned_alphabet))
-                .and(plaintext())
-                .and(with_source(owned_data_source))
-                .and_then(push_handler),
-        );
+        .and(warp::path!(String))
+        .then(redirect_handler)
+        .or(warp::get().then(empty_redirect_handler));
+    let push = warp::put()
+        .and(warp::path::end())
+        .and(warp::body::content_length_limit(4096))
+        .and(check_auth())
+        .and(plaintext())
+        .and_then(push_handler);
+    let push_custom = warp::put()
+        .and(warp::path!("custom" / String))
+        .and(warp::body::content_length_limit(4096))
+        .and(check_auth())
+        .and(plaintext())
+        .and_then(push_custom_handler);
 
-    let filters = redirect.or(push);
+    let filters = redirect.or(push).or(push_custom).recover(handle_rejection);
 
     println!("Listening on port {}", port);
     warp::serve(filters).run(([0, 0, 0, 0], port)).await;
 }
+
+// TODO handle rejections
 
 #[cfg(test)]
 mod tests {
